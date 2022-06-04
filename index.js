0df1ecf98b1af38f7bcfe44bc8e95ad1bef371fa
Gaurav <gaurav.rpandit1@gmail.com>
2022-06-04 20:09:25 +0530

    Server Deployment rerouting

--- index.js
+++ index.js
@@ -22,13 +22,19 @@ mongoose.connect(process.env.MONGO_URL, {
     console.log(err.message);
 });
 
+app.use(function (req, res, next) {
+    res.header("Access-Control-Allow-Origin", "https://smart-room-chat.herokuapp.com");
+    res.header("Access-Control-Allow-Headers","Origin, X-Requested-With, Content-Type, Accept");
+    next();   
+})
+
 const server = app.listen(process.env.PORT, ()=>{
     console.log("Server at Port:"+process.env.PORT);
 });
 
 const io = socket(server, {
     cors:{
-        origin: "http://localhost:3000",
+        origin: "https://smart-room-chat.herokuapp.com/",
         credentials: true
     }
 })
