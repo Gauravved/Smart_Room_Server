156fe1aa52207f4abe5550d321d2de2aaba4f3cf
Gaurav <gaurav.rpandit1@gmail.com>
2022-06-04 22:48:01 +0530

    Server Cors error solving

--- index.js
+++ index.js
@@ -23,8 +23,9 @@ mongoose.connect(process.env.MONGO_URL, {
 });
 
 app.use(function (req, res, next) {
-    res.header("Access-Control-Allow-Origin", "*");
-    res.header("Access-Control-Allow-Headers","Origin, X-Requested-With, Content-Type, Accept");
+    res.header("Access-Control-Allow-Origin: https://smart-room-chat.herokuapp.com ");
+    res.header("Access-Control-Allow-Headers: Origin, X-Requested-With, Content-Type, X-Auth-Token");
+    res.header("Access-Control-Allow-Methods: GET, POST, PUT, PATCH, DELETE, OPTIONS");
     next();   
 })
 
