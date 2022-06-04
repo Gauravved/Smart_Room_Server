2d1480fb6e2a32a42899f235aa99d63da27ea7e0
Gaurav <gaurav.rpandit1@gmail.com>
2022-06-04 22:50:29 +0530

    Server Cors error solving

--- index.js
+++ index.js
@@ -8,8 +8,16 @@ const User = require('./models/userModel').userModel;
 
 const app = express();
 require('dotenv').config();
-app.use(cors());
+// app.use(cors());
 app.use(express.json());
+
+
+// app.use(function (req, res, next) {
+//     res.header("Access-Control-Allow-Origin: https://smart-room-chat.herokuapp.com ");
+//     res.header("Access-Control-Allow-Headers: Origin, X-Requested-With, Content-Type, X-Auth-Token");
+//     res.header("Access-Control-Allow-Methods: GET, POST, PUT, PATCH, DELETE, OPTIONS");
+//     next();   
+// });
 app.use('/api/auth', userRoute);
 app.use('/api/message', messageRoute);
 
@@ -22,12 +30,6 @@ mongoose.connect(process.env.MONGO_URL, {
     console.log(err.message);
 });
 
-app.use(function (req, res, next) {
-    res.header("Access-Control-Allow-Origin: https://smart-room-chat.herokuapp.com ");
-    res.header("Access-Control-Allow-Headers: Origin, X-Requested-With, Content-Type, X-Auth-Token");
-    res.header("Access-Control-Allow-Methods: GET, POST, PUT, PATCH, DELETE, OPTIONS");
-    next();   
-})
 
 const server = app.listen(process.env.PORT, ()=>{
     console.log("Server at Port:"+process.env.PORT);
