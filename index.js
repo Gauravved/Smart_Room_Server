865f7e8e4af6d043aa09f2bc32426e83d2fe0a71
Gaurav <gaurav.rpandit1@gmail.com>
2022-06-04 23:27:52 +0530

    Server Cors error solving

--- index.js
+++ index.js
@@ -13,10 +13,10 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 
 
-app.use(function (req, res, next) {
-    res.header("Access-Control-Allow-Origin: https://smart-room-chat.herokuapp.com");
-    res.header("Access-Control-Allow-Headers: Origin, X-Requested-With, Content-Type, X-Auth-Token");
-    res.header("Access-Control-Allow-Methods: GET, POST, PUT, PATCH, DELETE, OPTIONS");
+app.use(function(req, res, next) {
+    res.header("Access-Control-Allow-Origin","https://smart-room-chat.herokuapp.com");
+    res.header("Access-Control-Allow-Headers","Origin, X-Requested-With, Content-Type, X-Auth-Token");
+    res.header("Access-Control-Allow-Methods","GET, POST, PUT, PATCH, DELETE, OPTIONS");
     next();   
 });
 app.use('/api/auth', userRoute);
