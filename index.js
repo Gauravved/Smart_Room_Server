732a3cda31f3b660dbb82243262600ffa92bb01a
Gaurav <gaurav.rpandit1@gmail.com>
2022-06-04 20:11:21 +0530

    Server Deployment rerouting

--- index.js
+++ index.js
@@ -23,7 +23,7 @@ mongoose.connect(process.env.MONGO_URL, {
 });
 
 app.use(function (req, res, next) {
-    res.header("Access-Control-Allow-Origin", "https://smart-room-chat.herokuapp.com");
+    res.header("Access-Control-Allow-Origin", "*");
     res.header("Access-Control-Allow-Headers","Origin, X-Requested-With, Content-Type, Accept");
     next();   
 })
