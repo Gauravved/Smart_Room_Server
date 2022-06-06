3e507157aca848be19ff2aeecdf5c2a6316e86b6
Gaurav <gaurav.rpandit1@gmail.com>
2022-06-06 13:22:50 +0530

    Trying to solve cors error

--- index.js
+++ index.js
@@ -16,7 +16,6 @@ app.use(cors({
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 
-app.use(express.static(path.join(__dirname+"/public/build")));
 app.use(function(req, res, next) {
     res.header("Access-Control-Allow-Origin","*");
     res.header("Access-Control-Allow-Headers","Origin, X-Requested-With, Content-Type, X-Auth-Token");
