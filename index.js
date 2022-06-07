ae8cf258be5e797273aa54167c69d5ea60fa55dd
Gaurav <gaurav.rpandit1@gmail.com>
2022-06-07 20:09:35 +0530

    Cors error almost solved

--- index.js
+++ index.js
@@ -18,11 +18,11 @@ app.all('/*',function(req, res, next) {
     res.header("Access-Control-Allow-Methods","GET, POST, PUT, PATCH, DELETE, OPTIONS");
     next();   
 });
-app.use(express.static(path.join(__dirname + '/public'+'/build')));
+// app.use(express.static(path.join(__dirname + '/public'+'/build')));
 
-app.use('/', (req,res,next)=>{
-    res.sendFile('./public/build');
-})
+// app.use('/', (req,res,next)=>{
+//     res.sendFile('./public/build');
+// })
 app.use(function(req, res, next) {
     res.header("Access-Control-Allow-Origin","*");
     res.header("Access-Control-Allow-Methods","GET, POST, PUT, PATCH, DELETE, OPTIONS");
