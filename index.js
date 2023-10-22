12c12896c6225b2977dc0fe0d406984cff622c29
Gaurav <gaurav.rpandit1@gmail.com>
2023-10-22 23:36:10 +0530

    Deployment to vercel

--- index.js
+++ index.js
@@ -18,7 +18,7 @@ app.use(cors());
 app.options('*',cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
-app.use((req, res, next) => {
+app.use("/*",(req, res, next) => {
     res.header('Access-Control-Allow-Origin', 'https://smartroom-kappa.vercel.app'); // You can specify your allowed origin here
     res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
     res.header('Access-Control-Allow-Headers', 'Content-Type');
@@ -55,7 +55,7 @@ server.listen(process.env.PORT,()=>{
 // })
 const io = socket(server, {
     cors:{
-        origin: 'https://smartroom-kappa.vercel.app/',
+        origin: 'https://smartroom-kappa.vercel.app',
         credential: true
     }
 });
