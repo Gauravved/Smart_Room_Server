e0fe89d84865825c5aadd1bbbb2a9d2fd8a94698
Gaurav <gaurav.rpandit1@gmail.com>
2023-10-22 23:05:43 +0530

    Deployment to vercel

--- index.js
+++ index.js
@@ -18,21 +18,24 @@ app.use(cors());
 app.options('*',cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
-app.all('/*',function(req, res, next) {
-    res.header("Access-Control-Allow-Origin","*");
-    res.header("Access-Control-Allow-Methods","GET, POST, PUT, PATCH, DELETE, OPTIONS");
-    next();   
-});
+app.use((req, res, next) => {
+    res.header('Access-Control-Allow-Origin', 'https://smartroom-kappa.vercel.app'); // You can specify your allowed origin here
+    res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
+    res.header('Access-Control-Allow-Headers', 'Content-Type');
+    res.header('Access-Control-Allow-Credentials', 'true');
+    res.header('Access-Control-Max-Age', '3600'); // Optional
+    next();
+  });
 // app.use(express.static(path.join(__dirname + '/public'+'/build')));
 
 // app.use('/', (req,res,next)=>{
 //     res.sendFile('./public/build');
 // })
-app.use(function(req, res, next) {
-    res.header("Access-Control-Allow-Origin","*");
-    res.header("Access-Control-Allow-Methods","GET, POST, PUT, PATCH, DELETE, OPTIONS");
-    next();   
-});
+// app.use(function(req, res, next) {
+//     res.header("Access-Control-Allow-Origin","*");
+//     res.header("Access-Control-Allow-Methods","GET, POST, PUT, PATCH, DELETE, OPTIONS");
+//     next();   
+// });
 app.use('/api/auth', userRoute);
 app.use('/api/message', messageRoute);
 mongoose.connect(process.env.MONGO_URL, {
